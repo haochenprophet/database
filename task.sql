eda4199c641bc3102665e1ddc9f1049abd8f1e4c
hunter <hao__chen@sina.com>
2017-06-13 11:11:57 +0800

    modified:   task.sql

--- task.sql
+++ task.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `task` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE  IF NOT EXISTS `task` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `task`;
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.13, for linux-glibc2.5 (x86_64)
 --
 -- Host: localhost    Database: task
 -- ------------------------------------------------------
--- Server version	8.0.0-dmr-log
+-- Server version	5.7.18-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,63 @@ USE `task`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `schedule`
+--
+
+DROP TABLE IF EXISTS `schedule`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schedule` (
+  `idschedule` int(11) NOT NULL AUTO_INCREMENT,
+  `taskid` int(11) NOT NULL,
+  `when` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'When to complete this task?',
+  `who` varchar(45) DEFAULT NULL COMMENT 'Who accomplishes this task?',
+  `what` text COMMENT 'What is this task?\n',
+  `where` text COMMENT 'Where to finish the task?',
+  `remark` text,
+  PRIMARY KEY (`idschedule`,`taskid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schedule`
+--
+
+LOCK TABLES `schedule` WRITE;
+/*!40000 ALTER TABLE `schedule` DISABLE KEYS */;
+/*!40000 ALTER TABLE `schedule` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tasks` (
+  `idtake` int(11) NOT NULL,
+  `content` text COMMENT 'What is the task content?',
+  `owner` varchar(45) DEFAULT NULL COMMENT 'Who is the owner of the task',
+  `sponsor` varchar(45) DEFAULT NULL COMMENT 'Task sponsor',
+  `start` datetime DEFAULT NULL COMMENT 'Task start time.',
+  `end` datetime DEFAULT NULL COMMENT 'Task end time',
+  `where` text COMMENT 'Where the task. ',
+  `remark` text,
+  PRIMARY KEY (`idtake`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -26,4 +83,4 @@ USE `task`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-04-18 11:42:53
+-- Dump completed on 2017-06-13 11:11:21
