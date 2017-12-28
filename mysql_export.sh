07aa87ce4addba48b6179ac4c54fac508d06085b
haochenprophet <hao.chen@prophet.net.cn>
2017-12-28 15:20:16 +0800

    Update db export

--- mysql_export.sh
+++ mysql_export.sh
@@ -1,21 +1,46 @@
+echo aiproject
 mysqldump -u root -p aiproject >aiproject_d.sql
+echo audio
 mysqldump -u root -p audio >audio_d.sql
+echo bookmark
 mysqldump -u root -p bookmark >bookmark_d.sql
+echo chip
 mysqldump -u root -p chip >chip_d.sql
+echo code
 mysqldump -u root -p code >code_d.sql
+echo company
 mysqldump -u root -p company >company_d.sql
+echo file
 mysqldump -u root -p file >file_d.sql
+echo iwant
 mysqldump -u root -p iwant >iwant_d.sql
+echo knowledge
 mysqldump -u root -p knowledge >knowledge_d.sql
+echo law
 mysqldump -u root -p law >law_d.sql
+echo life
 mysqldump -u root -p life >life_d.sql
+echo medical
 mysqldump -u root -p medical >medical_d.sql
+echo mysql
 mysqldump -u root -p mysql >mysql_d.sql
+echo patent
 mysqldump -u root -p patent >patent_d.sql
+echo question
 mysqldump -u root -p question >question_d.sql
+echo radar
 mysqldump -u root -p radar >radar_d.sql
+echo search
 mysqldump -u root -p search >search_d.sql
+echo task
 mysqldump -u root -p task >task_d.sql
+echo template
 mysqldump -u root -p template >template_d.sql
+echo user
 mysqldump -u root -p user >user_d.sql
+echo vedio
 mysqldump -u root -p vedio >vedio_d.sql
+echo debt
+mysqldump -u root -p debt >debt_d.sql
+echo video
+mysqldump -u root -p video >video_d.sql
