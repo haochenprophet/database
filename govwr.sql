9c522de072df8cffc65b9d2b3c4f1bf2ab83c238
haochenprophet <hao.chen@prophet.net.cn>
2018-01-02 14:50:18 +0800

    Add govwr db

---
+++ govwr.sql
@@ -0,0 +1,72 @@
+CREATE DATABASE  IF NOT EXISTS `govwr` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `govwr`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: govwr
+-- ------------------------------------------------------
+-- Server version	8.0.0-dmr-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `file`
+--
+
+DROP TABLE IF EXISTS `file`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `file` (
+  `idfile` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL COMMENT 'name=title+uuid',
+  `uuid` varchar(45) NOT NULL DEFAULT 'UUID()',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(45) NOT NULL COMMENT 'name=./org/author/title+uuid',
+  `type` varchar(45) DEFAULT NULL COMMENT 'txt,doc,xml,audio,vedio,pdf ,html,',
+  `author` varchar(45) NOT NULL,
+  `tel` varchar(45) DEFAULT NULL,
+  `mail` varchar(45) DEFAULT NULL,
+  `org` varchar(45) NOT NULL,
+  `where` varchar(45) NOT NULL,
+  `md5` varchar(45) NOT NULL COMMENT 'file md5 checksum',
+  `compress` tinyint(4) NOT NULL DEFAULT '0',
+  `cpsw` varchar(45) DEFAULT NULL COMMENT 'Compress password',
+  `secret` int(11) NOT NULL DEFAULT '0' COMMENT 'Secret level',
+  `spsw` varchar(45) DEFAULT NULL COMMENT 'Secret level''s password',
+  `decrypt` datetime DEFAULT NULL COMMENT 'decrypt =decrypt time +password',
+  `status` varchar(45) DEFAULT NULL,
+  `introduction` text,
+  `remark` text,
+  PRIMARY KEY (`idfile`,`uuid`),
+  UNIQUE KEY `idfile_UNIQUE` (`idfile`),
+  UNIQUE KEY `uuid_UNIQUE` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='government documents';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `file`
+--
+
+LOCK TABLES `file` WRITE;
+/*!40000 ALTER TABLE `file` DISABLE KEYS */;
+/*!40000 ALTER TABLE `file` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-02 14:49:16
