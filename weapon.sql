c6ef420c1887ac3a72755bb7ecafd0f615f37a45
haochenprophet <hao.chen@prophet.net.cn>
2018-01-18 16:31:18 +0800

    backup db

--- weapon.sql
+++ weapon.sql
@@ -37,7 +37,7 @@ CREATE TABLE `url` (
   `priority` int(11) NOT NULL DEFAULT '0',
   `where` varchar(45) NOT NULL DEFAULT 'china',
   PRIMARY KEY (`idurl`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='URL links .';
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COMMENT='URL links .';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,6 +46,7 @@ CREATE TABLE `url` (
 
 LOCK TABLES `url` WRITE;
 /*!40000 ALTER TABLE `url` DISABLE KEYS */;
+INSERT INTO `url` VALUES (1,'hunter','spacex','website','http://www.spacex.com/','img/spacex.jpg','2018-01-18 14:10:49','normal','utf8',100,'china'),(2,'hunter','lockheedmartin','website','http://www.lockheedmartin.com/','img/LM-logo.png','2018-01-18 14:19:17','normal','utf8',99,'china'),(3,'hunter','boeing','website','http://www.boeing.com/','img/boeing.png','2018-01-18 14:22:41','normal','utf8',98,'china'),(4,'hunter','nasa','website','https://www.nasa.gov/','img/nasa.jpg','2018-01-18 14:25:36','normal','utf8',97,'china'),(5,'hunter','raytheon','website','https://www.raytheon.com/','img/rtn_logo.gif','2018-01-18 14:33:14','normal','utf8',96,'china'),(6,'hunter','northropgrumman','website','http://www.northropgrumman.com','img/northropgrumman.png','2018-01-18 14:43:49','normal','utf8',95,'china'),(7,'hunter','generaldynamics','website','http://www.generaldynamics.com/','img/generaldynamics.jpg','2018-01-18 14:51:03','normal','utf8',94,'china'),(8,'hunter','l3t','website','https://www.l3t.com/','img/l3-logo.png','2018-01-18 14:56:20','normal','utf8',93,'china'),(9,'hunter','utc','website','http://www.utc.com','img/utc.jpg','2018-01-18 14:59:48','normal','utf8',92,'china'),(10,'hunter','baesystems','website','https://www.baesystems.com','img/baesystems.png','2018-01-18 15:03:21','normal','utf8',92,'china'),(11,'hunter','baesystems','website','http://www.airbus.com/','img/Airbus.png','2018-01-18 15:07:16','normal','utf8',91,'china'),(12,'hunter','ge','website','https://www.ge.com/','img/ge.jpg','2018-01-18 15:14:43','normal','utf8',90,'china'),(13,'hunter','mhi','website','https://www.mhi.com/','img/mhi.png','2018-01-18 15:20:03','normal','utf8',89,'china'),(14,'hunter','rheinmetall','website','https://www.rheinmetall.com','img/rheinmetall.jpg','2018-01-18 15:27:23','normal','utf8',88,'china'),(15,'hunter','heckler-koch','website','https://www.heckler-koch.com','img/heckler-koch.jpg','2018-01-18 15:30:50','normal','utf8',87,'china'),(16,'hunter','dassault-aviation','website','https://www.dassault-aviation.com','img/Dassault-Aviation.png','2018-01-18 15:35:11','normal','utf8',86,'china'),(17,'hunter','OKA','website','http://www.uacrussia.ru','img/OKA.png','2018-01-18 15:44:53','normal','utf8',101,'china'),(18,'hunter','rafael','website','http://www.rafael.co.il','img/rafael.png','2018-01-18 15:58:10','normal','utf8',86,'china'),(19,'hunter','iai','website','http://www.iai.co.il/','img/iai.png','2018-01-18 16:02:02','normal','utf8',86,'china'),(20,'hunter','imisystems','website','http://www.imisystems.com/','img/IMISystems.png','2018-01-18 16:05:03','normal','utf8',86,'china'),(21,'hunter','elbitsystems','website','http://elbitsystems.com/','img/elbitsystems.png','2018-01-18 16:07:16','normal','utf8',86,'china'),(22,'hunter','Leonardo','website','http://www.leonardocompany.com','img/Leonardo.png','2018-01-18 16:18:18','normal','utf8',85,'china'),(23,'hunter','bel','website','http://bel-india.com/','img/bel.jpg','2018-01-18 16:25:11','normal','utf8',84,'china'),(24,'hunter','electricdatasystems','website','http://www.electricdatasystems.com/','img/EDS-logo.png','2018-01-18 16:28:53','normal','utf8',83,'china');
 /*!40000 ALTER TABLE `url` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -58,4 +59,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-17 17:28:38
+-- Dump completed on 2018-01-18 16:29:50
