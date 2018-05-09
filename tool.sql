4ecf2d765d8a978c9b3be2e0cfda33a1b934cc80
haochenprophet <hao.chen@prophet.net.cn>
2018-05-09 17:20:37 +0800

    backup tool db

--- tool.sql
+++ tool.sql
@@ -28,15 +28,16 @@ CREATE TABLE `url` (
   `idurl` int(11) NOT NULL AUTO_INCREMENT,
   `who` varchar(45) DEFAULT NULL COMMENT 'who create the link item .',
   `what` varchar(45) NOT NULL COMMENT 'what the link item information ?',
-  `what_type` varchar(45) DEFAULT NULL,
-  `where` varchar(200) NOT NULL COMMENT 'where the URL address',
+  `type` varchar(45) DEFAULT NULL,
+  `url` varchar(200) NOT NULL COMMENT 'where the URL address',
   `logo` varchar(45) NOT NULL COMMENT 'where the URL logo path',
   `when` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'the link create time .',
   `status` varchar(45) DEFAULT NULL,
   `remark` varchar(45) DEFAULT NULL,
   `priority` int(11) NOT NULL DEFAULT '0',
+  `where` varchar(45) NOT NULL DEFAULT 'china',
   PRIMARY KEY (`idurl`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='konwledge URL links .';
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COMMENT='konwledge URL links .';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,6 +46,7 @@ CREATE TABLE `url` (
 
 LOCK TABLES `url` WRITE;
 /*!40000 ALTER TABLE `url` DISABLE KEYS */;
+INSERT INTO `url` VALUES (1,'hunter','runoob','website','https://c.runoob.com/','img/runoob_word.jpg','2017-12-29 17:01:41','normal','utf8',0,'china'),(2,'hunter','tortoisesvn','website','https://tortoisesvn.net/','img/tortoisesvn.jpg','2017-12-29 17:12:35','normal','utf8',0,'china'),(3,'hunter','git','website','https://git-scm.com/','img/git.jpg','2017-12-29 17:14:29','normal','utf8',0,'china'),(4,'hunter','intel','website','https://downloadcenter.intel.com/','img/intel.jpg','2018-01-02 17:46:49','normal','utf8',0,'china'),(5,'hunter','asus','website','https://www.asus.com/support/','img/asus.jpg','2018-01-02 17:53:39','normal','utf8',0,'china'),(6,'hunter','visualstudio','website','https://www.visualstudio.com','img/visualstudio.jpg','2018-01-02 18:02:01','normal','utf8',0,'china'),(7,'hunter','apache','website','https://www.apache.org/index.html#projects-list','img/asf_logo.png','2018-01-02 18:09:19','normal','utf8',0,'china'),(8,'hunter','eclipse','website','http://www.eclipse.org/','img/eclipse.png','2018-01-02 18:16:05','normal','utf8',0,'china'),(9,'hunter','sublime','website','https://www.sublimetext.com/','img/sublime.jpg','2018-01-02 18:23:03','normal','utf8',0,'china'),(10,'hunter','solidworks','website','http://www.solidworks.com/','img/solidworks.png','2018-01-08 18:04:08','normal','china',0,'china'),(11,'hunter','cadence','website','https://www.cadence.com/','img/cadence.jpg','2018-01-08 18:10:56','normal','china',0,'china'),(12,'hunter','xcmgmall','website','http://www.xcmgmall.com','img/xcmg.jpg','2018-01-08 18:16:58','normal','china',0,'china'),(13,'hunter','openssl','website','https://www.openssl.org/','img/openssl.jpg','2018-01-09 10:47:00','normal','china',0,'china'),(14,'hunter','xilinx','website','http://www.xilinx.com/','img/xilinx.jpeg','2018-01-10 14:08:29','normal','china',0,'china'),(15,'hunter','swift','website','https://swift.org/','img/swift.jpg','2018-05-09 17:01:28','normal','utf8',0,'china'),(16,'hunter','bc_compare','website','http://scootersoftware.com/','img/bc_compare.jpg','2018-05-09 17:11:16','normal','utf8',0,'china'),(17,'hunter','netbeans','website','https://netbeans.org/','img/netbeans.jpg','2018-05-09 17:15:15','normal','utf8',0,'china'),(18,'hunter','pycharm','website','https://www.jetbrains.com/pycharm/','img/pycharm.jpg','2018-05-09 17:18:33','normal','utf8',0,'china');
 /*!40000 ALTER TABLE `url` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -57,4 +59,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-29 16:32:50
+-- Dump completed on 2018-05-09 17:19:54
