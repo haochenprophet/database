e5c7d7df177975241488e602ace42246bd8ff368
haochenprophet <hao.chen@prophet.net.cn>
2018-12-19 16:13:58 +0800

    Add people db

---
+++ people.sql
@@ -0,0 +1,92 @@
+CREATE DATABASE  IF NOT EXISTS `people` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `people`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: people
+-- ------------------------------------------------------
+-- Server version	8.0.0-dmr-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `people`
+--
+
+DROP TABLE IF EXISTS `people`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `people` (
+  `idpeople` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(45) NOT NULL,
+  `id_card` varchar(45) DEFAULT NULL COMMENT 'ID card',
+  `time` varchar(45) NOT NULL DEFAULT 'NOW()',
+  `who` varchar(45) DEFAULT NULL COMMENT ' who is he/She?',
+  `when` varchar(45) DEFAULT NULL COMMENT 'When was born',
+  `which` varchar(45) DEFAULT NULL COMMENT 'which country',
+  `where` varchar(100) DEFAULT NULL COMMENT 'where address',
+  `gender` varchar(45) DEFAULT NULL COMMENT 'What gender',
+  `title` varchar(100) DEFAULT NULL COMMENT 'Job title',
+  `job` varchar(500) DEFAULT NULL COMMENT 'Where is the job?',
+  `religion` varchar(45) DEFAULT NULL COMMENT 'What religion',
+  `party` varchar(45) DEFAULT NULL COMMENT 'what political party',
+  `credit` varchar(45) DEFAULT NULL COMMENT 'Social credit',
+  `wish` varchar(300) DEFAULT NULL COMMENT 'What is wish?',
+  `influence` varchar(300) DEFAULT NULL COMMENT 'How big is social influence?',
+  `achievement` varchar(300) DEFAULT NULL COMMENT 'What is the achievement?',
+  `email` varchar(100) DEFAULT NULL COMMENT 'What is the email address?',
+  `tel` varchar(45) DEFAULT NULL COMMENT 'telephone',
+  `wealth` varchar(100) DEFAULT NULL COMMENT 'How much is personal wealth?',
+  `priority` int(11) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `evaluate` varchar(300) DEFAULT NULL,
+  `record` text COMMENT 'Bad or good record',
+  `remark` text,
+  `url` varchar(200) DEFAULT NULL COMMENT 'Personal website',
+  `photo` varchar(200) DEFAULT NULL COMMENT 'photo url',
+  `blog` varchar(200) DEFAULT NULL COMMENT 'blog url',
+  `audio` varchar(200) DEFAULT NULL COMMENT 'audio url ',
+  `video` varchar(200) DEFAULT NULL COMMENT 'video url',
+  `living` tinyint(4) NOT NULL DEFAULT '1',
+  `death_time` datetime DEFAULT NULL COMMENT 'What time of death',
+  `testament` text,
+  `testament_time` datetime DEFAULT NULL,
+  `history` text COMMENT ' history',
+  `case_history` text COMMENT 'case history',
+  `insurance` text,
+  `family` varchar(500) DEFAULT NULL,
+  `organization` varchar(300) DEFAULT NULL,
+  `land` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`idpeople`,`uuid`),
+  UNIQUE KEY `idpeople_UNIQUE` (`idpeople`),
+  UNIQUE KEY `uuid_UNIQUE` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='people';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `people`
+--
+
+LOCK TABLES `people` WRITE;
+/*!40000 ALTER TABLE `people` DISABLE KEYS */;
+/*!40000 ALTER TABLE `people` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-12-19 16:13:18
