762c171ec2b1eacdd539284bc315406bb575ac1e
haochenprophet <hao.chen@prophet.net.cn>
2019-01-16 15:58:43 +0800

    Add sop db

---
+++ sop.sql
@@ -0,0 +1,92 @@
+CREATE DATABASE  IF NOT EXISTS `sop` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `sop`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sop
+-- ------------------------------------------------------
+-- Server version	8.0.0-dmr-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sop`
+--
+
+DROP TABLE IF EXISTS `sop`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sop` (
+  `idsop` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(45) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `item` varchar(1024) DEFAULT NULL,
+  `step` double DEFAULT NULL,
+  `input` varchar(1024) DEFAULT NULL,
+  `operate` varchar(1024) DEFAULT NULL,
+  `output` varchar(1024) DEFAULT NULL,
+  `archive` varchar(1024) DEFAULT NULL,
+  `archive_type` varchar(45) DEFAULT NULL,
+  `remark` varchar(1024) DEFAULT NULL,
+  `author` varchar(45) DEFAULT 'hunter',
+  `status` varchar(45) DEFAULT 'normal',
+  `priority` int(11) DEFAULT '0',
+  PRIMARY KEY (`idsop`,`uuid`),
+  UNIQUE KEY `idsop_UNIQUE` (`idsop`),
+  UNIQUE KEY `uuid_UNIQUE` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='sop';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sop`
+--
+
+LOCK TABLES `sop` WRITE;
+/*!40000 ALTER TABLE `sop` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sop` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `uuid`
+--
+
+DROP TABLE IF EXISTS `uuid`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `uuid` (
+  `iduuid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(45) NOT NULL,
+  `name` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`iduuid`,`uuid`),
+  UNIQUE KEY `idsop_item_UNIQUE` (`iduuid`),
+  UNIQUE KEY `uuid_UNIQUE` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='uuid';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `uuid`
+--
+
+LOCK TABLES `uuid` WRITE;
+/*!40000 ALTER TABLE `uuid` DISABLE KEYS */;
+/*!40000 ALTER TABLE `uuid` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-01-16 15:57:56
