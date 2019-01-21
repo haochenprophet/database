6896ebcef25e77f683a6826efaf4ed41654722ed
haochenprophet <hao.chen@prophet.net.cn>
2019-01-21 17:10:00 +0800

    backup uuid sop db

--- sop.sql
+++ sop.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `sop` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `sop`;
+CREATE DATABASE  IF NOT EXISTS `uuid` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `uuid`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: sop
+-- Host: localhost    Database: uuid
 -- ------------------------------------------------------
 -- Server version	8.0.0-dmr-log
 
@@ -18,75 +18,29 @@ USE `sop`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `sop`
+-- Table structure for table `type`
 --
 
-DROP TABLE IF EXISTS `sop`;
+DROP TABLE IF EXISTS `type`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sop` (
-  `idsop` int(11) NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(45) NOT NULL,
-  `item` varchar(256) NOT NULL,
-  `class` varchar(45) NOT NULL DEFAULT 'all',
+CREATE TABLE `type` (
+  `idtype` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
   `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `step` double DEFAULT NULL,
-  `input` varchar(1024) DEFAULT NULL,
-  `operate` varchar(1024) DEFAULT NULL,
-  `output` varchar(1024) DEFAULT NULL,
-  `archive` varchar(1024) DEFAULT NULL,
-  `archive_type` varchar(45) DEFAULT NULL,
-  `archive_md5` varchar(45) DEFAULT NULL,
-  `remark` varchar(1024) DEFAULT NULL,
-  `author` varchar(45) DEFAULT 'hunter',
-  `status` varchar(45) DEFAULT 'normal',
-  `priority` int(11) DEFAULT '0',
-  PRIMARY KEY (`idsop`,`uuid`,`item`),
-  UNIQUE KEY `idsop_UNIQUE` (`idsop`),
-  UNIQUE KEY `uuid_UNIQUE` (`uuid`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='sop';
+  PRIMARY KEY (`idtype`,`name`),
+  UNIQUE KEY `idtype_UNIQUE` (`idtype`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `sop`
+-- Dumping data for table `type`
 --
 
-LOCK TABLES `sop` WRITE;
-/*!40000 ALTER TABLE `sop` DISABLE KEYS */;
-INSERT INTO `sop` VALUES (1,'3071e9af-a6b4-47bf-945c-4cab3c418a72','账单发票票据表单填写提交流程','all','2019-01-17 16:10:53',1,'准备条件','发票的电子文件，图片，照片\r\n发票号码，申请人身份证号码','','','','','','hunter','normal',0),(2,'432b8df3-f769-4781-9adf-a220584d71ce','账单发票票据表单填写提交流程','all','2019-01-17 16:12:11',2,'进入网址','http://www.red-arrows.cn/bill_add.php','','','','','','hunter','normal',0),(3,'c32854dc-6c76-486b-8b8d-7f697bf17e1f','账单发票票据表单填写提交流程','all','2019-01-17 16:22:23',3,'填写表单','选择发票文件，填写发票日期，发票号码InvoiceNumber，\r\n姓名who，身份证号码IDcard,发票产生地址where，大概事由what，\r\n资金流向to，\r\n发票金额amount 注意+-，备注','','','','','发票金额amount 流入公司为正+，否则为负','hunter','normal',0),(4,'059e077a-173e-4ecc-9eef-66865b9abe7a','账单发票票据表单填写提交流程','all','2019-01-17 16:27:24',4,'提交表单','1.点击提交按钮提交表单\r\n','查看是否生成记录','','','','如果遇到错误刷新填写页面重新填写','hunter','normal',0);
-/*!40000 ALTER TABLE `sop` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `url`
---
-
-DROP TABLE IF EXISTS `url`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `url` (
-  `idurl` int(11) NOT NULL AUTO_INCREMENT,
-  `who` varchar(45) DEFAULT NULL COMMENT 'who create the link item .',
-  `what` varchar(45) NOT NULL COMMENT 'what the link item information ?',
-  `type` varchar(45) DEFAULT NULL,
-  `url` varchar(200) NOT NULL COMMENT 'where the URL address',
-  `logo` varchar(45) NOT NULL COMMENT 'where the URL logo path',
-  `when` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'the link create time .',
-  `status` varchar(45) DEFAULT NULL,
-  `remark` varchar(45) DEFAULT NULL,
-  `priority` int(11) NOT NULL DEFAULT '0',
-  `where` varchar(45) NOT NULL DEFAULT 'china',
-  PRIMARY KEY (`idurl`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='URL links .';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `url`
---
-
-LOCK TABLES `url` WRITE;
-/*!40000 ALTER TABLE `url` DISABLE KEYS */;
-/*!40000 ALTER TABLE `url` ENABLE KEYS */;
+LOCK TABLES `type` WRITE;
+/*!40000 ALTER TABLE `type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `type` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -105,7 +59,7 @@ CREATE TABLE `uuid` (
   PRIMARY KEY (`iduuid`,`uuid`,`name`),
   UNIQUE KEY `idsop_item_UNIQUE` (`iduuid`),
   UNIQUE KEY `uuid_UNIQUE` (`uuid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='uuid';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='uuid';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -114,7 +68,6 @@ CREATE TABLE `uuid` (
 
 LOCK TABLES `uuid` WRITE;
 /*!40000 ALTER TABLE `uuid` DISABLE KEYS */;
-INSERT INTO `uuid` VALUES (1,'3071e9af-a6b4-47bf-945c-4cab3c418a72','账单发票票据表单填写提交流程','all','2019-01-17 16:10:53');
 /*!40000 ALTER TABLE `uuid` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -127,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-17 17:16:07
+-- Dump completed on 2019-01-21 17:09:21
--- uuid.sql
+++ uuid.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `uuid` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `uuid`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: uuid
+-- Host: localhost    Database: uuid
 -- ------------------------------------------------------
 -- Server version	8.0.0-dmr-log
 
@@ -16,6 +16,32 @@ USE `uuid`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `type`
+--
+
+DROP TABLE IF EXISTS `type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `type` (
+  `idtype` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idtype`,`name`),
+  UNIQUE KEY `idtype_UNIQUE` (`idtype`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `type`
+--
+
+LOCK TABLES `type` WRITE;
+/*!40000 ALTER TABLE `type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `type` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `uuid`
@@ -54,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-17 15:05:42
+-- Dump completed on 2019-01-21 17:03:41
