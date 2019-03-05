6f90138ae8d43e7fd5e9c1a1694f545fe558b3c4
haochenprophet <hao.chen@prophet.net.cn>
2019-03-05 14:50:41 +0800

    Add industry_chain.sql db

---
+++ industry_chain.sql
@@ -0,0 +1,157 @@
+CREATE DATABASE  IF NOT EXISTS `industry_chain` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `industry_chain`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: industry_chain
+-- ------------------------------------------------------
+-- Server version	8.0.0-dmr-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `chain`
+--
+
+DROP TABLE IF EXISTS `chain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chain` (
+  `idchain` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(45) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `sart` datetime DEFAULT NULL,
+  `end` datetime DEFAULT NULL,
+  `when` datetime DEFAULT NULL,
+  `where` varchar(200) DEFAULT NULL COMMENT 'space',
+  `who` varchar(100) DEFAULT NULL COMMENT 'enterprise',
+  `what` varchar(200) DEFAULT NULL,
+  `value` varchar(200) DEFAULT NULL COMMENT 'value description',
+  `from` varchar(200) DEFAULT NULL COMMENT 'supply from',
+  `to` varchar(200) DEFAULT NULL COMMENT 'supply to',
+  `amount` decimal(50,2) DEFAULT NULL,
+  `price` decimal(50,2) DEFAULT NULL,
+  `remark` varchar(200) DEFAULT NULL,
+  `priority` int(11) NOT NULL DEFAULT '0',
+  `status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idchain`,`uuid`),
+  UNIQUE KEY `uuid_UNIQUE` (`uuid`),
+  UNIQUE KEY `idchain_UNIQUE` (`idchain`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chain`
+--
+
+LOCK TABLES `chain` WRITE;
+/*!40000 ALTER TABLE `chain` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chain` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `enterprise`
+--
+
+DROP TABLE IF EXISTS `enterprise`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `enterprise` (
+  `identerprise` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`identerprise`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `enterprise`
+--
+
+LOCK TABLES `enterprise` WRITE;
+/*!40000 ALTER TABLE `enterprise` DISABLE KEYS */;
+/*!40000 ALTER TABLE `enterprise` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `space`
+--
+
+DROP TABLE IF EXISTS `space`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `space` (
+  `idspace` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`idspace`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `space`
+--
+
+LOCK TABLES `space` WRITE;
+/*!40000 ALTER TABLE `space` DISABLE KEYS */;
+/*!40000 ALTER TABLE `space` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `supply`
+--
+
+DROP TABLE IF EXISTS `supply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `supply` (
+  `idsupply` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`idsupply`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='supply';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `supply`
+--
+
+LOCK TABLES `supply` WRITE;
+/*!40000 ALTER TABLE `supply` DISABLE KEYS */;
+/*!40000 ALTER TABLE `supply` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `value`
+--
+
+DROP TABLE IF EXISTS `value`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `value` (
+  `idvalue` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`idvalue`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='value';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `value`
+--
+
+LOCK TABLES `value` WRITE;
+/*!40000 ALTER TABLE `value` DISABLE KEYS */;
+/*!40000 ALTER TABLE `value` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-03-05 14:46:53
