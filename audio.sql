1f50e6b650a9f302148a48cbb9cc0f3372ad01ae
hunter <hao__chen@sina.com>
2019-03-15 10:50:22 +0800

    backup db

--- audio.sql
+++ audio.sql
@@ -1,15 +1,15 @@
 CREATE DATABASE  IF NOT EXISTS `audio` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `audio`;
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: audio
+-- Host: localhost    Database: audio
 -- ------------------------------------------------------
--- Server version	8.0.0-dmr-log
+-- Server version	8.0.13-commercial
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+ SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,18 +23,19 @@ USE `audio`;
 
 DROP TABLE IF EXISTS `url`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `url` (
   `idurl` int(11) NOT NULL AUTO_INCREMENT,
   `who` varchar(45) DEFAULT NULL COMMENT 'who create the link item .',
   `what` varchar(45) NOT NULL COMMENT 'what the link item information ?',
-  `what_type` varchar(45) DEFAULT NULL,
-  `where` varchar(200) NOT NULL COMMENT 'where the URL address',
+  `type` varchar(45) DEFAULT NULL,
+  `url` varchar(200) NOT NULL COMMENT 'where the URL address',
   `logo` varchar(45) NOT NULL COMMENT 'where the URL logo path',
   `when` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'the link create time .',
   `status` varchar(45) DEFAULT NULL,
   `remark` varchar(45) DEFAULT NULL,
   `priority` int(11) NOT NULL DEFAULT '0',
+  `where` varchar(45) NOT NULL DEFAULT 'china',
   PRIMARY KEY (`idurl`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='konwledge URL links .';
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -45,7 +46,7 @@ CREATE TABLE `url` (
 
 LOCK TABLES `url` WRITE;
 /*!40000 ALTER TABLE `url` DISABLE KEYS */;
-INSERT INTO `url` VALUES (1,'hunter','ximalaya','audio','http://www.ximalaya.com/','img/ximalaya.jpg','2017-12-30 01:33:21','normal','utf8',0),(2,'hunter','music.163','music','http://music.163.com/','img/music.163.jpg','2017-12-30 01:44:04','normal','',0),(3,'hunter','kugou','music','http://www.kugou.com/','img/kugo.jpg','2017-12-30 01:48:15','normal','utf8',0),(4,'hunter','qqmusic','music','https://y.qq.com/','img/qqmusic.jpg','2017-12-30 02:03:27','normal','utf8',0);
+INSERT INTO `url` VALUES (1,'hunter','ximalaya','audio','http://www.ximalaya.com/','img/ximalaya.jpg','2017-12-30 01:33:21','normal','utf8',0,'china'),(2,'hunter','music.163','music','http://music.163.com/','img/music.163.jpg','2017-12-30 01:44:04','normal','',0,'china'),(3,'hunter','kugou','music','http://www.kugou.com/','img/kugo.jpg','2017-12-30 01:48:15','normal','utf8',0,'china'),(4,'hunter','qqmusic','music','https://y.qq.com/','img/qqmusic.jpg','2017-12-30 02:03:27','normal','utf8',0,'china');
 /*!40000 ALTER TABLE `url` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -58,4 +59,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-05 17:38:22
+-- Dump completed on 2019-03-15 10:50:03
