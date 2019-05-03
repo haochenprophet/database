5dcf947c15f3782ac1015ee2da091ee06b776518
hunter <hao__chen@sina.com>
2019-05-03 10:26:44 +0800

    Add egov db

---
+++ egov.sql
@@ -0,0 +1,62 @@
+CREATE DATABASE  IF NOT EXISTS `egov` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `egov`;
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
+--
+-- Host: localhost    Database: egov
+-- ------------------------------------------------------
+-- Server version	8.0.13-commercial
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `url`
+--
+
+DROP TABLE IF EXISTS `url`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `url` (
+  `idurl` int(11) NOT NULL AUTO_INCREMENT,
+  `who` varchar(45) DEFAULT NULL COMMENT 'who create the link item .',
+  `what` varchar(100) NOT NULL COMMENT 'what the link item information ?',
+  `type` varchar(45) DEFAULT NULL,
+  `url` varchar(200) NOT NULL COMMENT 'where the URL address',
+  `logo` varchar(200) NOT NULL COMMENT 'where the URL logo path',
+  `when` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'the link create time .',
+  `status` varchar(45) DEFAULT NULL,
+  `remark` varchar(200) DEFAULT NULL,
+  `priority` int(11) NOT NULL DEFAULT '0',
+  `where` varchar(45) NOT NULL DEFAULT 'china',
+  PRIMARY KEY (`idurl`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COMMENT='URL links .';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `url`
+--
+
+LOCK TABLES `url` WRITE;
+/*!40000 ALTER TABLE `url` DISABLE KEYS */;
+INSERT INTO `url` VALUES (1,'198382.com','online-court','website','https://github.com/online-court','img/egov.png','2019-05-03 10:13:45','normal','utf8',0,'china'),(2,'198382.com','open-water','website','https://github.com/open-water','img/egov.png','2019-05-03 10:16:14','normal','utf8',0,'china'),(3,'198382.com','open-debt','website','https://github.com/open-debt','img/egov.png','2019-05-03 10:16:42','normal','utf8',0,'china'),(4,'198382.com','ai-water','website','https://github.com/ai-water','img/egov.png','2019-05-03 10:17:22','normal','utf8',0,'china'),(5,'198382.com','ihistory','website','https://github.com/ihistory','img/egov.png','2019-05-03 10:17:47','normal','utf8',0,'china'),(6,'198382.com','open-egov','website','https://github.com/open-egov','img/egov.png','2019-05-03 10:18:27','normal','utf8',100,'china'),(7,'198382.com','open-origin','website','https://github.com/open-origin','img/egov.png','2019-05-03 10:19:01','normal','utf8',0,'china'),(8,'198382.com','Personal Lifetime Information Database','website','https://github.com/PLID','img/egov.png','2019-05-03 10:20:27','normal','utf8',0,'china'),(9,'198382.com','patent-sys','website','https://github.com/patent-sys','img/egov.png','2019-05-03 10:20:56','normal','utf8',0,'china'),(10,'198382.com','open-war','website','https://github.com/open-war','img/egov.png','2019-05-03 10:21:49','normal','utf8',0,'china'),(11,'198382.com','free-office','website','https://github.com/free-office','img/egov.png','2019-05-03 10:22:59','normal','utf8',0,'china');
+/*!40000 ALTER TABLE `url` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-05-03 10:26:12
