95999aaea2c4bfba82fd1190877c9c0cdb8a284a
hao.chen <hao__chen@sina.com>
2019-05-12 12:47:06 +0800

    Fixed import fail of linux OS

--- org.sql
+++ org.sql
@@ -31,7 +31,7 @@ CREATE TABLE `org` (
   PRIMARY KEY (`idorg`,`name`),
   UNIQUE KEY `idorg_UNIQUE` (`idorg`),
   UNIQUE KEY `org_UNIQUE` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- wage.sql
+++ wage.sql
@@ -57,7 +57,7 @@ CREATE TABLE `org` (
   PRIMARY KEY (`idorg`,`name`),
   UNIQUE KEY `idorg_UNIQUE` (`idorg`),
   UNIQUE KEY `org_UNIQUE` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
