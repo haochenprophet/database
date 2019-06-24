6b4b19ae228cd93bb30c78d00649a4e0abea568e
chenhaohaochenlulu <xhfch@126.com>
2019-06-24 15:30:24 +0800

    Add where db

--- materiel.sql
+++ materiel.sql
@@ -25,19 +25,31 @@ DROP TABLE IF EXISTS `materiel`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `materiel` (
-  `idmateriel` int(11) NOT NULL AUTO_INCREMENT,
+  `idmateriel` bigint(20) NOT NULL AUTO_INCREMENT,
   `uuid` varchar(45) NOT NULL,
   `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `type` varchar(45) NOT NULL COMMENT 'Category',
   `barcode` varchar(45) NOT NULL,
   `serial_number` varchar(45) NOT NULL,
   `part_number` varchar(45) NOT NULL,
+  `date_code` varchar(45) NOT NULL,
   `name` varchar(100) NOT NULL,
   `manufacturer` varchar(100) DEFAULT NULL,
-  `status` varchar(45) NOT NULL COMMENT 'Stock Status',
-  `where` varchar(45) NOT NULL COMMENT 'location',
+  `when` varchar(45) DEFAULT NULL,
+  `where` varchar(200) NOT NULL COMMENT 'location',
+  `who` varchar(45) DEFAULT NULL,
   `what` varchar(45) DEFAULT NULL COMMENT 'what project',
-  PRIMARY KEY (`idmateriel`,`uuid`,`time`,`type`,`barcode`,`serial_number`,`part_number`,`name`),
+  `why` varchar(45) DEFAULT NULL,
+  `how` varchar(45) DEFAULT NULL,
+  `country` varchar(45) DEFAULT NULL,
+  `site` varchar(45) DEFAULT NULL,
+  `location` varchar(45) DEFAULT NULL,
+  `from` varchar(45) DEFAULT NULL,
+  `to` varchar(45) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `status` varchar(45) NOT NULL COMMENT 'Stock Status',
+  `remark` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`idmateriel`,`uuid`,`time`,`type`,`barcode`,`serial_number`,`part_number`,`date_code`,`name`),
   UNIQUE KEY `idmateriel_UNIQUE` (`idmateriel`),
   UNIQUE KEY `uuid_UNIQUE` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -61,4 +73,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-21 11:12:08
+-- Dump completed on 2019-06-24 14:48:09
