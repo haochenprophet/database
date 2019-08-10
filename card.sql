59476de16679702ff295665c3fcfeca41d650a95
hunter <hao__chen@sina.com>
2019-08-10 14:42:03 +0800

    backup card db

---
+++ card.sql
@@ -0,0 +1,178 @@
+CREATE DATABASE  IF NOT EXISTS `card` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `card`;
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
+--
+-- Host: localhost    Database: card
+-- ------------------------------------------------------
+-- Server version	8.0.13-commercial
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `card`
+--
+
+DROP TABLE IF EXISTS `card`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `card` (
+  `idcard` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `when` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `end` datetime DEFAULT NULL,
+  `where` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `who` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `owner` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `what` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `number` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `type` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `from` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `to` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `why` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `how` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `status` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `priority` int(11) NOT NULL DEFAULT '0',
+  `remark` varchar(300) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`idcard`,`uuid`,`time`,`when`,`who`,`owner`,`number`),
+  UNIQUE KEY `idcard_UNIQUE` (`idcard`),
+  UNIQUE KEY `uuid_UNIQUE` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `card`
+--
+
+LOCK TABLES `card` WRITE;
+/*!40000 ALTER TABLE `card` DISABLE KEYS */;
+/*!40000 ALTER TABLE `card` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `status` (
+  `idstatus` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `time` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idstatus`,`name`),
+  UNIQUE KEY `idstatus_UNIQUE` (`idstatus`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status`
+--
+
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `type`
+--
+
+DROP TABLE IF EXISTS `type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `type` (
+  `idtype` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idtype`,`name`),
+  UNIQUE KEY `idtype_UNIQUE` (`idtype`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `type`
+--
+
+LOCK TABLES `type` WRITE;
+/*!40000 ALTER TABLE `type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `what`
+--
+
+DROP TABLE IF EXISTS `what`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `what` (
+  `idwhat` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idwhat`,`name`),
+  UNIQUE KEY `idwhat_UNIQUE` (`idwhat`),
+  UNIQUE KEY `what_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `what`
+--
+
+LOCK TABLES `what` WRITE;
+/*!40000 ALTER TABLE `what` DISABLE KEYS */;
+/*!40000 ALTER TABLE `what` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `who`
+--
+
+DROP TABLE IF EXISTS `who`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `who` (
+  `idwho` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `name_en` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `name_cn` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `org` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'organization',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idwho`,`ID`,`name_en`,`name_cn`),
+  UNIQUE KEY `idwho_UNIQUE` (`idwho`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`),
+  UNIQUE KEY `name_en_UNIQUE` (`name_en`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `who`
+--
+
+LOCK TABLES `who` WRITE;
+/*!40000 ALTER TABLE `who` DISABLE KEYS */;
+/*!40000 ALTER TABLE `who` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-08-10 14:41:40
