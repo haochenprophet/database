88f46eca761766e6c42dbda7a71fcc8f7a286158
chenhaohaochenlulu <xhfch@126.com>
2019-09-23 23:29:45 +0800

    backup history db

--- history.sql
+++ history.sql
@@ -18,6 +18,52 @@ USE `history`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `history`
+--
+
+DROP TABLE IF EXISTS `history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `history` (
+  `idhistory` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(45) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `title` varchar(200) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `when` datetime DEFAULT CURRENT_TIMESTAMP,
+  `what` varchar(500) NOT NULL,
+  `text` varchar(500) DEFAULT NULL,
+  `url` varchar(300) DEFAULT NULL,
+  `priority` int(11) NOT NULL DEFAULT '0',
+  `status` varchar(45) DEFAULT NULL,
+  `remark` varchar(500) DEFAULT NULL,
+  `video` varchar(300) DEFAULT NULL,
+  `audio` varchar(300) DEFAULT NULL,
+  `image` varchar(300) DEFAULT NULL,
+  `who` varchar(100) DEFAULT NULL,
+  `file` varchar(500) DEFAULT NULL,
+  `where` varchar(100) DEFAULT NULL,
+  `nation` varchar(100) DEFAULT NULL COMMENT 'country',
+  `region` varchar(200) DEFAULT NULL COMMENT 'area',
+  `why` varchar(500) DEFAULT NULL,
+  `how` varchar(500) DEFAULT NULL COMMENT 'how to do',
+  `evaluate` varchar(500) DEFAULT NULL,
+  `author` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idhistory`,`uuid`,`time`,`title`,`type`,`what`,`priority`),
+  UNIQUE KEY `uuid_UNIQUE` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `history`
+--
+
+LOCK TABLES `history` WRITE;
+/*!40000 ALTER TABLE `history` DISABLE KEYS */;
+/*!40000 ALTER TABLE `history` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `status`
 --
 
@@ -42,39 +88,6 @@ LOCK TABLES `status` WRITE;
 /*!40000 ALTER TABLE `status` DISABLE KEYS */;
 /*!40000 ALTER TABLE `status` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `history`
---
-
-DROP TABLE IF EXISTS `history`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `history` (
-  `idhistory` bigint(20) NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(45) NOT NULL,
-  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `title` varchar(200) NOT NULL,
-  `type` varchar(45) NOT NULL,
-  `what` varchar(45) NOT NULL,
-  `text` varchar(500) DEFAULT NULL,
-  `url` varchar(300) DEFAULT NULL,
-  `priority` int(11) NOT NULL DEFAULT '0',
-  `status` varchar(45) DEFAULT NULL,
-  `remark` varchar(300) DEFAULT NULL,
-  `video` varchar(300) DEFAULT NULL,
-  `audio` varchar(300) DEFAULT NULL,
-  `image` varchar(300) DEFAULT NULL,
-  `who` varchar(100) DEFAULT NULL,
-  `where` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`idhistory`,`uuid`,`time`,`title`,`priority`,`type`,`what`),
-  UNIQUE KEY `uuid_UNIQUE` (`uuid`)
-) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `history`
---
 
 --
 -- Table structure for table `type`
@@ -117,12 +130,17 @@ CREATE TABLE `what` (
   PRIMARY KEY (`idwhat`,`name`),
   UNIQUE KEY `idwhat_UNIQUE` (`idwhat`),
   UNIQUE KEY `what_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `what`
 --
+
+LOCK TABLES `what` WRITE;
+/*!40000 ALTER TABLE `what` DISABLE KEYS */;
+/*!40000 ALTER TABLE `what` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -133,4 +151,4 @@ CREATE TABLE `what` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-04 16:59:24
+-- Dump completed on 2019-09-23 23:28:34
