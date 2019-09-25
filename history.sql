8ead1429a037674c502660ba5a4eeae7568cffc8
chenhaohaochenlulu <xhfch@126.com>
2019-09-25 16:15:35 +0800

    backup history db

--- history.sql
+++ history.sql
@@ -53,7 +53,7 @@ CREATE TABLE `history` (
   `file` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`idhistory`,`uuid`,`time`,`when`,`end`,`what`,`title`,`type`,`priority`),
   UNIQUE KEY `uuid_UNIQUE` (`uuid`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,7 +62,7 @@ CREATE TABLE `history` (
 
 LOCK TABLES `history` WRITE;
 /*!40000 ALTER TABLE `history` DISABLE KEYS */;
-INSERT INTO `history` VALUES (1,'458f4335-75bf-41dc-b976-fe080c54753f','2019-09-24 17:37:44','2019-09-24 09:33:32','2019-09-24 09:33:32','建立全球统一记录历史数据库','记录历史','科技','用数据库记录历史','苏州','中国','江苏','赤箭智能科技','人类历史和事件记录混乱','帮助人类更加准确的记录历史','推动使用全球统一历史记录数据库应用',0,'check in','','Hunter','https://www.red-arrows.cn/history.php','','','','',''),(2,'767b5e19-590c-4f7a-800e-4758b738eca4','2019-09-25 13:42:20','2019-09-25 13:36:35','2019-09-25 13:36:35','北京大兴国际机场正式投运','北京大兴国际机场','经济','北京大兴国际机场正式投运','北京大兴国际机场','中国','北京','中国','北京国际航班众多，提升北京航空运载能力','加速北京航空运输能力','加强中国多用途机场建设，提升航运能力',0,'check in','','Hunter','https://www.thepaper.cn/newsDetail_forward_4519795','','','','','');
+INSERT INTO `history` VALUES (1,'458f4335-75bf-41dc-b976-fe080c54753f','2019-09-24 17:37:44','2019-09-24 09:33:32','2019-09-24 09:33:32','建立全球统一记录历史数据库','记录历史','科技','用数据库记录历史','苏州','中国','江苏','赤箭智能科技','人类历史和事件记录混乱','帮助人类更加准确的记录历史','推动使用全球统一历史记录数据库应用',0,'check in','','Hunter','https://www.red-arrows.cn/history.php','','','','',''),(2,'767b5e19-590c-4f7a-800e-4758b738eca4','2019-09-25 13:42:20','2019-09-25 13:36:35','2019-09-25 13:36:35','北京大兴国际机场正式投运','北京大兴国际机场','经济','北京大兴国际机场正式投运','北京大兴国际机场','中国','北京','中国','北京国际航班众多，提升北京航空运载能力','加速北京航空运输能力','加强中国多用途机场建设，提升航运能力',0,'check in','','Hunter','https://www.thepaper.cn/newsDetail_forward_4519795','','','','',''),(3,'8af5f884-e994-457d-92e2-71273a5a34f1','2019-09-25 14:44:28','2019-09-25 14:37:42','2019-09-25 14:37:42','阿里巴巴旗下半导体公司平头哥发布AI推理芯片含光800','阿里巴巴旗下半导体公司平头哥发布AI推理芯片含光800','科技','平头哥成立以来发布的首款硬件产品，一秒可以处理7.8万张图片。','杭州云栖大会','中国','杭州','阿里巴巴达摩院','阿里巴巴转向技术驱动，进军全栈,芯片,系统,应用技术行列','中国芯片行业，正由市场主流的科技公司推动，逐步形成自成体系的产业','开放技术，构建生态，产生价值，形成商机，',0,'check in','','Hunter','https://www.thepaper.cn/newsDetail_forward_4520260','','','','','');
 /*!40000 ALTER TABLE `history` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -154,4 +154,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-25 14:03:43
+-- Dump completed on 2019-09-25 16:15:07
