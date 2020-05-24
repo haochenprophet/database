82c9530a20387412dbc28428a4859adc8788ae17
chenhaohaochenlulu <xhfch@126.com>
2020-05-24 11:43:33 +0800

    backup biosecurity.sql db

--- biosecurity.sql
+++ biosecurity.sql
@@ -18,6 +18,60 @@ USE `biosecurity`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `report`
+--
+
+DROP TABLE IF EXISTS `report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `report` (
+  `idreport` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(45) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `when` datetime NOT NULL,
+  `where` varchar(400) NOT NULL COMMENT 'detailed address',
+  `nation` varchar(100) NOT NULL,
+  `province` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `organization` varchar(400) NOT NULL COMMENT 'Hospital name',
+  `who` varchar(45) NOT NULL COMMENT 'Patient name',
+  `id_card` varchar(45) NOT NULL,
+  `phone` varchar(45) DEFAULT NULL COMMENT 'Patient''s contact phone number',
+  `address` varchar(45) DEFAULT NULL COMMENT 'Patient''s contact address',
+  `profession` varchar(45) DEFAULT NULL,
+  `place` varchar(45) DEFAULT NULL COMMENT 'Work place',
+  `guardian` varchar(45) DEFAULT NULL COMMENT 'Guardian name',
+  `contact` varchar(450) DEFAULT NULL COMMENT 'Guardian Contact',
+  `what` varchar(800) DEFAULT NULL COMMENT 'Content of report',
+  `why` varchar(800) DEFAULT NULL COMMENT 'cause of disease',
+  `etiology` varchar(100) DEFAULT NULL COMMENT 'Virus name, bacteria name, ',
+  `contagious` varchar(45) DEFAULT NULL COMMENT 'Is it contagious',
+  `how` varchar(800) DEFAULT NULL COMMENT 'How to deal with and solve\n',
+  `level` varchar(45) NOT NULL DEFAULT 'normal' COMMENT 'emergency level:normal,high,low',
+  `amount` int(11) NOT NULL DEFAULT '1',
+  `reporter` varchar(200) DEFAULT NULL COMMENT 'reporter',
+  `telephone` varchar(45) DEFAULT NULL COMMENT 'reporter telephone',
+  `priority` int(11) DEFAULT '0' COMMENT 'Priority of feedback processing',
+  `status` varchar(45) DEFAULT NULL COMMENT 'Current processing status',
+  `remark` varchar(1000) DEFAULT NULL,
+  `feedback` varchar(1000) DEFAULT NULL,
+  `result` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`idreport`,`uuid`,`time`,`when`,`nation`,`province`,`city`,`organization`,`who`,`id_card`),
+  UNIQUE KEY `idreport_UNIQUE` (`idreport`),
+  UNIQUE KEY `uuid_UNIQUE` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT=' Direct Report System';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `report`
+--
+
+LOCK TABLES `report` WRITE;
+/*!40000 ALTER TABLE `report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `report` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `url`
 --
 
@@ -49,6 +103,44 @@ LOCK TABLES `url` WRITE;
 INSERT INTO `url` VALUES (1,'198382.com','CDC_GOV','website','https://www.cdc.gov/','img/biosecurity.jpg','2020-03-21 14:02:02','normal','utf8',0,'china'),(2,'198382.com','CDC_CN','website','http://www.chinacdc.cn/','img/biosecurity.jpg','2020-03-21 14:04:12','normal','utf8',0,'china'),(3,'198382.com','WHO','website','https://www.who.int/','img/who.jpg','2020-03-21 14:10:19','normal','utf8',0,'china'),(4,'198382.com','中草药(Chinese Herbal Antidote)','website','http://www.tiprpress.com','img/herbs.jpg','2020-03-24 18:54:53','normal','utf8',0,'china');
 /*!40000 ALTER TABLE `url` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `virus`
+--
+
+DROP TABLE IF EXISTS `virus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `virus` (
+  `idvirus` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(45) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(200) NOT NULL,
+  `type` varchar(45) NOT NULL COMMENT 'Animals, plants, bacteria, viruses',
+  `structure` varchar(45) NOT NULL COMMENT 'Biological structure Single-stranded RNA viruses, double-stranded RNA viruses, single-stranded DNA viruses and double-stranded DNA viruses',
+  `contagious` varchar(45) NOT NULL COMMENT 'Is it contagious',
+  `conceal` int(11) NOT NULL DEFAULT '0' COMMENT 'Virus incubation period',
+  `level` int(11) NOT NULL DEFAULT '0' COMMENT 'Levels of danger',
+  `infectiousness` varchar(1000) DEFAULT NULL COMMENT 'infect way',
+  `origin` varchar(1000) DEFAULT NULL COMMENT 'Virus origin',
+  `who` varchar(45) DEFAULT NULL COMMENT 'author',
+  `status` varchar(45) DEFAULT NULL,
+  `remark` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`idvirus`,`uuid`,`time`,`name`,`type`,`structure`,`contagious`),
+  UNIQUE KEY `idvirus_UNIQUE` (`idvirus`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  UNIQUE KEY `uuid_UNIQUE` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virus`
+--
+
+LOCK TABLES `virus` WRITE;
+/*!40000 ALTER TABLE `virus` DISABLE KEYS */;
+/*!40000 ALTER TABLE `virus` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -59,4 +151,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-24 19:00:49
+-- Dump completed on 2020-05-24 11:42:52
