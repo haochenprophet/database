1467349f444695461a30fc6f6f9257085a36525c
hunter <hao__chen@sina.com>
2020-05-30 09:09:11 +0800

    backup gov db

---
+++ gov.sql
@@ -0,0 +1,115 @@
+CREATE DATABASE  IF NOT EXISTS `gov` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `gov`;
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gov
+-- ------------------------------------------------------
+-- Server version	8.0.13-commercial
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `item` (
+  `iditem` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(45) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `item` varchar(1024) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `goal` varchar(1024) NOT NULL,
+  `earn` double NOT NULL DEFAULT '0' COMMENT 'Donor income',
+  `priority` int(11) NOT NULL DEFAULT '0',
+  `creator` varchar(1024) NOT NULL,
+  `executor` varchar(1024) NOT NULL,
+  `info` varchar(1024) DEFAULT NULL,
+  `start` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'star time',
+  `end` datetime DEFAULT NULL COMMENT 'end time',
+  `request` varchar(1024) DEFAULT NULL,
+  `method` varchar(1024) NOT NULL,
+  `price` double NOT NULL DEFAULT '0',
+  `father` varchar(45) NOT NULL DEFAULT '0' COMMENT 'depend father item uuid',
+  `child` varchar(45) NOT NULL DEFAULT '0' COMMENT 'child itme uuid',
+  `url` varchar(1024) NOT NULL COMMENT 'url ',
+  `path` varchar(1024) DEFAULT NULL COMMENT 'path of workspace',
+  `size` bigint(20) NOT NULL DEFAULT '107374182400' COMMENT 'path space size ',
+  `image` varchar(1024) DEFAULT NULL COMMENT 'file name ',
+  `audio` varchar(1024) DEFAULT NULL COMMENT 'file name',
+  `video` varchar(1024) DEFAULT NULL COMMENT 'file name',
+  `group` varchar(1024) DEFAULT NULL COMMENT 'work group',
+  `address` varchar(1024) DEFAULT NULL,
+  `mail` varchar(1024) DEFAULT NULL,
+  `tel` varchar(1024) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `remark` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`iditem`,`uuid`),
+  UNIQUE KEY `uuid_UNIQUE` (`uuid`),
+  UNIQUE KEY `iditem_UNIQUE` (`iditem`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `url`
+--
+
+DROP TABLE IF EXISTS `url`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `url` (
+  `idurl` int(11) NOT NULL AUTO_INCREMENT,
+  `who` varchar(45) DEFAULT NULL COMMENT 'who create the link item .',
+  `what` varchar(45) NOT NULL COMMENT 'what the link item information ?',
+  `type` varchar(45) DEFAULT NULL,
+  `url` varchar(200) NOT NULL COMMENT 'where the URL address',
+  `logo` varchar(45) NOT NULL COMMENT 'where the URL logo path',
+  `when` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'the link create time .',
+  `status` varchar(45) DEFAULT NULL,
+  `remark` varchar(45) DEFAULT NULL,
+  `priority` int(11) NOT NULL DEFAULT '0',
+  `where` varchar(45) NOT NULL DEFAULT 'china',
+  PRIMARY KEY (`idurl`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COMMENT='URL links .';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `url`
+--
+
+LOCK TABLES `url` WRITE;
+/*!40000 ALTER TABLE `url` DISABLE KEYS */;
+INSERT INTO `url` VALUES (1,'hunter','usa','website','https://www.usa.gov/','img/usa.jpg','2018-02-09 22:56:34','normal','utf8',100,'china'),(2,'hunter','china','website','http://www.gov.cn/','img/china.jpg','2018-02-09 22:59:59','normal','utf8',99,'china'),(3,'hunter','russia','website','http://government.ru/','img/russia.png','2018-02-09 23:03:57','normal','utf8',98,'china'),(4,'hunter','canada','website','https://www.canada.ca/en.html','img/Canada.jpg','2018-02-09 23:07:32','normal','utf8',97,'china'),(5,'hunter','India','website','https://www.india.gov.in/','img/India.jpg','2018-02-09 23:08:47','normal','utf8',96,'china'),(6,'hunter','UnitedKingdom','website','https://www.gov.uk/','img/UnitedKingdom.jpg','2018-02-09 23:10:15','normal','utf8',95,'china'),(7,'hunter','Germany','website','https://www.bundesregierung.de/','img/Germany.jpg','2018-02-09 23:13:34','normal','utf8',94,'china'),(8,'hunter','france','website','http://www.gouvernement.fr/','img/france.jpg','2018-02-09 23:20:54','normal','utf8',93,'china'),(9,'hunter','Japan','website','https://nettv.gov-online.go.jp/','img/Japan.jpg','2018-02-09 23:22:30','normal','utf8',92,'china'),(10,'hunter','Vietnam','website','http://chinhphu.vn','img/Vietnam.jpg','2018-02-09 23:28:14','normal','utf8',91,'china'),(11,'hunter','Australia','website','https://www.gov.au/','img/Australia.jpg','2018-02-09 23:30:13','normal','utf8',90,'china'),(12,'hunter','Brazil','website','http://www.brasil.gov.br/','img/Brazil.jpg','2018-02-09 23:34:35','normal','utf8',89,'china');
+/*!40000 ALTER TABLE `url` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-30  9:08:39
